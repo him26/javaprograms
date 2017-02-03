8d60840c66d1fd75a8c18244bdad7d2b77a192f1
Himanshu Gautam <gautamhimanshu5446@gmail.com>
2017-02-03 18:49:55 +0530

    I modified something

--- javaprograms/src/com/bridgelab/programs/FindNumbers.java
+++ javaprograms/src/com/bridgelab/programs/FindNumbers.java
@@ -3,25 +3,38 @@ package com.bridgelab.programs;
 import java.util.*;
 public class FindNumbers
 {
-	public static boolean binarysearch(int[] A,int x,int high,int low)
+	public static boolean userans()
+	{
+		Scanner sc=new Scanner(System.in);
+		boolean rs=sc.nextBoolean();
+		return rs;
+	}
+	public static int search(int high,int low)
 	{
 		if(high>=low)	
 		{
 			int mid= (high+low)/2;
-			if(A[mid]==x)
+			System.out.println("is it your element    "+mid); 
+			boolean a=userans();
+			if(a)
 			{
-				return true;
+				System.out.println("you got your answer");
+				return mid;
 			}
-			if(A[mid]<x)		
+			System.out.println("is your element is lesser than   "+mid);
+			boolean b=userans();
+			if(b)		
 			{
-				return binarysearch(A, x, mid-1, low);		
+				return search(mid-1, low);		
 			}
-			if(A[mid]>x)	
+			System.out.println("is your element is greater than   "+mid);
+			boolean c=userans();
+			if(c)	
 			{
-				return binarysearch( A, x, high,mid+1);
+				return search(high,mid+1);
 			}
 		}
-		return false;
+		return -1;
 	}
 	Utility ul=new Utility();
 	public static void main(String[] args)
@@ -33,13 +46,6 @@ public class FindNumbers
 		int high=(int)Math.pow(2, num);
 		int low=0;
 		System.out.println("select a no between "+low+" to "+high);
-		int[] A=new int[high];
-		for (int i = 0; i < A.length; i++) 
-		{
-			A[i]=i;
-		}
-		int mid=(high+low)/2;
-		boolean rs=binarysearch(A, mid, high, low);
-		
+		search(high, low);
 	}
 }
